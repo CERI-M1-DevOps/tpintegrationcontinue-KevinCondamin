caf8127ce8f122822b26e4779fe98a028ae6f337
Kevin Condamin <kevin382000@gmail.com>
2025-09-11 15:42:29 +0200

    refactor: sonarqube revision

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -114,7 +114,7 @@ class ListeSimpleTest {
 
     @Test
     void supprimeTousListeVide() {
-        listeATester.supprimePremier(1);
+        listeATester.supprimeTous(1);
         assertNull(listeATester.tete);
         assertEquals(0, listeATester.getSize());
     }
